e880f5c08b2463ab44ab6255323d2cdc5c0656a5
Lyubov Rodomanova <lrodomanova81@gmail.com>
2022-04-07 21:48:07 +0300

    first

---
+++ src/guru/qa/Main.java
@@ -0,0 +1,16 @@
+package guru.qa;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int year = 2021;
+        double pi = 3.1415;
+        String name = "Java";
+        int x=5; int y=10;
+        boolean flag = x<y;
+        System.out.println(year);
+        System.out.println(pi);
+        System.out.println(name);
+        System.out.println(flag);
+    }
+}
