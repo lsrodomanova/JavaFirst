f05f1c6f28fe3f54743a6febfe3a8d1538852d93
Lyubov Rodomanova <lrodomanova81@gmail.com>
2022-04-19 16:13:20 +0300

    second

--- src/guru/qa/Main.java
+++ src/guru/qa/Main.java
@@ -5,12 +5,37 @@ public class Main {
     public static void main(String[] args) {
         int year = 2021;
         double pi = 3.1415;
-        String name = "Java";
+        String title = "Java";
+        long b = 922335452;
+        short c = -32768;
         int x=5; int y=10;
-        boolean flag = x<y;
-        System.out.println(year);
-        System.out.println(pi);
-        System.out.println(name);
+        int w=0;
+        boolean flag = x+y<b;
+        boolean flag2 = pi>b;
+        boolean flag3=(x+y)==(pi+year);
+        boolean flag4=(x+y)!=(pi+year);
+
+        System.out.println("a="+(year-pi));
+        System.out.println(title+pi);
+        System.out.println(pi*x);
+        System.out.println(pi/y);
+        System.out.println(b*b);
+        System.out.println(++x);
+        System.out.println(--y);
+        System.out.println(b/b);
+        System.out.println(year%x);
+        System.out.println(c-c);
         System.out.println(flag);
+        System.out.println(flag2);
+        System.out.println(flag3);
+        System.out.println(flag4);
+
+        if (pi+year>x+y) {
+            System.out.println("Это хороший пример");
+        }
+        else {
+            System.out.println("Ошибка");
+        }
     }
 }
+
